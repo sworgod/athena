edbe9ce808865c2a769fdd946c75c132b610408b
Alex Wang <sworgod@qq.com>
2021-10-20 05:32:55 +0800

    Add files via upload

---
+++ reinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash <(curl -s -L https://raw.githubusercontent.com/Sworgod/Athena/master/uninstall.sh)
+sleep 3
+bash <(curl -s -L https://raw.githubusercontent.com/Sworgod/Athena/master/install.sh)
