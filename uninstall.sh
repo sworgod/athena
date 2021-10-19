4ea9758970e9d51db72781297c0d2d11db71d021
Alex Wang <sworgod@qq.com>
2021-10-20 02:13:31 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,12 @@ chkconfig --del athena
 rm -rf /etc/init.d/athena
 rm -rf /usr/local/bin/athena
 rm -rf /usr/local/athena/
+
+if [ ! -f "/usr/sbin/firewalld" ]; then
+  iptables -D INPUT -p tcp -m state --state NEW -m tcp --dport 8120 -j ACCEPT
+  /etc/init.d/iptables save
+  /etc/init.d/iptables restart
+  else
+  firewall-cmd --permanent --zone=public --remove-port=8120/tcp
+  firewall-cmd --reload
+fi
