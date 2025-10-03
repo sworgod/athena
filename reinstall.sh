8668ad503d6473e2c579db98104b0f4ff0061b54
Alex <sworgod@qq.com>
2025-10-03 22:46:59 +0800

    1.6.10

--- reinstall.sh
+++ reinstall.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-service athena stop
-
-rm -rf /etc/init.d/athena
-rm -rf /usr/local/bin/athena
-rm -rf /usr/local/athena/athena
-
-echo -e "Delete Athena Success.\n"
-
+bash <(curl -s -L https://raw.githubusercontent.com/Sworgod/Athena/master/uninstall.sh)
 sleep 3
-
-wget -O /usr/local/athena/athena https://raw.githubusercontent.com/Sworgod/Athena/master/athena
-chmod 777 /usr/local/athena/athena
-ln -s /usr/local/athena/athena /usr/local/bin
-service athena start
+bash <(curl -s -L https://raw.githubusercontent.com/Sworgod/Athena/master/install.sh)
